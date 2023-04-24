cdc28fbba1e9e2eef6094f367f3a5d197ac8edd8
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2023-04-24 18:46:42 +0530

    Added Readme

--- src/main/java/com/myrestapi/myrestapi/repository/CustomerRepository.java
+++ src/main/java/com/myrestapi/myrestapi/repository/CustomerRepository.java
@@ -1,6 +1,15 @@
 package com.myrestapi.myrestapi.repository;
 
 import com.myrestapi.myrestapi.entity.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Integer>{}
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer>{
+    @Query("SELECT t FROM Customer t WHERE t.email=:email")
+    List<Customer> findByEmail(String email);
+
+}
+
+
